9f8bc5784f26272c5be0e5d32a3c662fbb82b1c0
Mansi Mandviya <95748400+mansimandviya@users.noreply.github.com>
2025-09-13 14:19:52 +0530

    Landing page UI

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import LandingPage from './pages/landing'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
