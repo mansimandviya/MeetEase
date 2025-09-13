2debc83d9ac5286dcb80fdb7bf531e69668b9809
Mansi Mandviya <95748400+mansimandviya@users.noreply.github.com>
2025-09-13 16:47:18 +0530

    Video call feature, chat feature, history feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import LandingPage from './pages/landing';
+import LandingPage from "./pages/landing";
 import Authentication from "./pages/authentication";
+import { AuthProvider } from "./contexts/AuthContext";
+import VideoMeetComponent from "./pages/videoMeet";
+import HomeComponent from "./pages/home";
+import History from "./pages/history";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path='/auth' element={<Authentication />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/auth" element={<Authentication />} />
+            <Route path="/home" s element={<HomeComponent />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/:url" element={<VideoMeetComponent />} />
+          </Routes>
+        </AuthProvider>
       </Router>
-    </>
+    </div>
   );
 }
 
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
 import "../App.css"
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'
 export default function LandingPage() {
-  return (
-    <div className='landingPageContainer'>
-        <nav>
+
+
+    const router = useNavigate();
+
+    return (
+        <div className='landingPageContainer'>
+            <nav>
                 <div className='navHeader'>
                     <h2>MeetEase Video Call</h2>
                 </div>
                 <div className='navlist'>
-                    <p>Join as Guest</p>
-                    <p>Register</p>
-                    <div role='button'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
                         <p>Login</p>
                     </div>
                 </div>
             </nav>
+
+
             <div className="landingMainContainer">
                 <div>
                     <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
@@ -31,6 +45,9 @@ export default function LandingPage() {
 
                 </div>
             </div>
-    </div>
-  )
+
+
+
+        </div>
+    )
 }
